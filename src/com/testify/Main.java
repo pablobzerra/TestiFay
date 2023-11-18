387eb53d58843985ae43a26523346ee2e3046eda
Pablo <pabloxgameplay@gmail.com>
2023-11-18 00:37:07 -0300

    Att

--- src/com/testify/Main.java
+++ src/com/testify/Main.java
@@ -1,5 +1,12 @@
 package com.testify;
 
+/*
+ * HELLO WORLD!! 
+ * 
+ * by Pablobzerra & Bruno(Radec)
+ * 
+ */
+
 public class Main {
     public static void main(String[] args) {
         //nada aqui por enquanto
